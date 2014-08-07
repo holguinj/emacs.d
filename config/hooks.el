9ae42eba6648f7646921de5128d1ce5800f3cecf
Justin Holguin <justin.holguin@puppetlabs.com>
2014-08-07 16:10:54 -0700

    Fuck you, ElDoc

--- config/hooks.el
+++ config/hooks.el
@@ -1,7 +1,6 @@
 (global-auto-complete-mode t)
 
 
-
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -16,3 +15,10 @@
 (add-hook 'clojure-mode-hook          #'rainbow-delimiters-mode)
 
 (add-hook 'markdown-mode-hook 'visual-line-mode)
+
+;; ElDoc mode SUCKS
+(defun disable-eldoc-mode ()
+  (eldoc-mode -1))
+
+(add-hook 'clojure-mode-hook          #'disable-eldoc-mode)
+(add-hook 'cider-repl-mode-hook       #'disable-eldoc-mode)
