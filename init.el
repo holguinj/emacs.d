18bdd4a4a19387a1c80be9c785727962c558d667
Justin Holguin <justin.holguin@puppetlabs.com>
2014-10-05 17:56:53 -0700

    Added hella settings to init.el

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (load "~/.emacs.d/config/evil-paredit")
 (load "~/.emacs.d/config/gui-settings")
 (load "~/.emacs.d/config/haskell")
+(load "~/.emacs.d/config/clj.el")
 (server-start)
 
 (custom-set-variables
@@ -19,10 +20,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-ghc-show-info t)
+ '(company-idle-delay 0.2)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
+ '(haskell-tags-on-save t)
  '(initial-buffer-choice t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#1b1d1e" :foreground "#f8f8f0")))))
+ '(default ((t (:background "#1b1d1e" :foreground "#f8f8f0"))))
+ '(company-scrollbar-bg ((t (:background "#2d3031"))))
+ '(company-tooltip ((t (:inherit default :background "#191b1c"))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
