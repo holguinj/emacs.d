589713ba4c514ca70995b1b26aa4b4257f9fe6bb
Justin Holguin <justin.holguin@puppetlabs.com>
2014-12-15 14:14:58 -0800

    Ugh ELPA I h8u

--- elpa/anaphora-20140508.1341/anaphora-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "anaphora" "20140508.1341" "anaphoric macros providing implicit temp variables" (quote nil))
--- elpa/hy-mode-20140201.2055/hy-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "hy-mode" "20140201.2055" "Major mode for Hy code" (quote nil))
