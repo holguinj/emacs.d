f1637e9dc5c731d85480c28502d2f42d0132b81d
Justin Holguin <justin.h.holguin@gmail.com>
2015-04-23 14:35:26 -0700

    disable paredit backslash

--- config/paredit-config.el
+++ config/paredit-config.el
@@ -6,7 +6,13 @@
 (define-key paredit-mode-map (kbd "C-M-e") 'paredit-forward-up)
 (define-key paredit-mode-map (kbd "C-M-s") 'paredit-backward-down)
 (define-key paredit-mode-map (kbd "C-M-d") 'paredit-backward-down)
+(define-key paredit-mode-map (kbd "\\") (lambda () (interactive) (insert "\\")))
 
+(defun disable-paredit-backslash ()
+  (local-set-key [remap paredit-backslash]
+                 (lambda ()
+                   (interactive)
+                   (insert "\\"))))
 
 ;; GANGSTA WRAP
 (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
