73804ff47106c55a4f9937bebe40cde0ee87ba01
Justin Holguin <justin.h.holguin@gmail.com>
2015-04-23 14:35:14 -0700

    attempt to refine helm settings

--- config/helm.el
+++ config/helm.el
@@ -2,3 +2,9 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (load "~/.emacs.d/lib/helm-projectile")
 (helm-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; helm-ag
+(setq helm-ag-insert-at-point nil)
+
+(setq helm-ag-fuzzy-match 't)
