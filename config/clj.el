a5faca093d749d67f9385c2c985a6dd75e6a3406
Justin Holguin <justin.h.holguin@gmail.com>
2015-04-23 14:34:47 -0700

    add highlighted words

--- config/clj.el
+++ config/clj.el
@@ -130,7 +130,7 @@
 (add-hook 'clojure-mode-hook
                (lambda ()
                 (font-lock-add-keywords nil
-                 '(("\\<\\(FIXME\\|TODO\\|BUG\\)" 1 font-lock-warning-face t)))))
+                 '(("\\<\\(FIXME\\|BREAK\\|BLOCK\\|TODO\\|BUG\\)" 1 font-lock-warning-face t)))))
 
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
